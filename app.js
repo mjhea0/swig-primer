0e078dc51bcffa0b3c5b88f9b7d3d8492e860da0
Michael Herman <hermanmu@gmail.com>
2015-08-20 11:33:29 -0600

    updated swig templates

--- app.js
+++ app.js
@@ -14,13 +14,39 @@ app.set('view engine', 'html');
 // *** static directory *** //
 app.set('views', path.join(__dirname, 'views'));
 
-
 // *** main routes *** //
 app.get('/', function(req, res) {
-  res.render('index.html');
+  var title = 'Swig Primer!'
+  var description = 'Swig is "a simple, powerful, and extendable JavaScript Template Engine" for NodeJS.'
+  function allthethings() {
+    return '<span>All the things!</span>';
+  }
+  res.render('index.html', {
+    title: title,
+    description: description,
+    allthethings: allthethings
+  });
+});
+app.get('/filter', function(req, res) {
+  res.render('filter.html', {
+    title: 'Hello, World!',
+    date: new Date(),
+    nameArray: ['This', 'is', 'just', 'an', 'example']
+  });
+});
+app.get('/logic', function(req, res) {
+  var title = 'Swig Logic!'
+  var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  res.render('logic.html', {title: title, numberArray: numberArray});
+});
+app.get('/test', function(req, res) {
+  var title = 'Inheritence in Action!'
+  res.render('test.html', {title: title});
 });
 
 // *** create server *** //
 var server = app.listen(3000, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
+
+
